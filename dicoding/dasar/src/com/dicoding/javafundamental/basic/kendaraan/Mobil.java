7522e2a1878da96113b0800acdad125941b12001
gallang sadewa <gallang1800016063@webmail.uad.ac.id>
2023-01-12 11:37:23 +0700

    amazing commit

---
+++ dicoding/dasar/src/com/dicoding/javafundamental/basic/kendaraan/Mobil.java
@@ -0,0 +1,11 @@
+package com.dicoding.javafundamental.basic.kendaraan;
+
+public class Mobil {
+    public static void jumlahBan() {
+        System.out.println("ban mobil 4");
+    }
+
+    public static void mesin() {
+        System.out.println("pake disel");
+    }
+}
---
+++ youtube/java-dasar/src/TipeDataNumber.java
@@ -0,0 +1,13 @@
+public class TipeDataNumber {
+
+    public static void main(String[] args) {
+
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 10000;
+        long iniLong = 10000000L;
+
+        float iniFloat = 10.10f;
+        double iniDouble = 10.10;
+    }
+}
